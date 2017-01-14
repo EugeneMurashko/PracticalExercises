55b95f8b80372e8d11c7b2d83fb7c827743035c2
EugeneMurahsko <eugen.murashko@gmail.com>
2017-01-15 00:59:47 +0300

    Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInfo User1 = new UserInfo();
+            UserInfo User2 = new UserInfo(Name: "Alex");
+            UserInfo User3 = new UserInfo(Name: "Alex", Age: 26);
+
+            Console.WriteLine("User1");
+            Console.WriteLine("Name: " + User1.Name);
+            Console.WriteLine("Age: " + User1.Age);
+            Console.WriteLine("");
+
+            Console.WriteLine("User2");
+            Console.WriteLine("Name: " + User2.Name);
+            Console.WriteLine("Age: " + User2.Age);
+            Console.WriteLine("");
+
+            Console.WriteLine("User3");
+            Console.WriteLine("Name: " + User3.Name);
+            Console.WriteLine("Age: " + User3.Age);
+
+            Console.ReadLine();
+        }
+    }
+
+    class UserInfo
+    {
+        public string Name { get; set; }
+        public byte Age { get; set; }
+
+        public UserInfo() { }
+        public UserInfo(string Name)
+        {
+            this.Name = Name;
+        }
+        public UserInfo(string Name, byte Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+    }
+}
